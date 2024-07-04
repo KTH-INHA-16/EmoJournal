58cd74ca20ea3a7b7737cc305b2e76ec2c1a0db3
KTH-INHA-16 <kim1120318@naver.com>
2024-07-04 15:42:53 +0900

    Initial Commit

---
+++ EmoJournal/EmoJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmoJournalApp.swift
+//  EmoJournal
+//
+//  Created by 김태훈 on 7/4/24.
+//
+
+import SwiftUI
+
+@main
+struct EmoJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
